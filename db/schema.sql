deb0ab0ad5d9644a6f6ff5c71d5cf390e9bbbab9
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-05-19 19:11:33 -0700

    db: postgres img with bootstrapped schema + data

---
+++ db/schema.sql
@@ -0,0 +1,154 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE SCHEMA enterprise;
+
+CREATE TABLE enterprise.employer (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    name TEXT NOT NULL,
+    location TEXT, -- TODO: for HQ. handle remote
+    url TEXT,
+
+    notes TEXT
+);
+
+-- Additional information about an employer.
+-- Examples: urls for glassdoor, stackshare, indeed, experiences, comments.
+CREATE TABLE enterprise.employer_resource (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    employer UUID REFERENCES enterprise.employer,
+
+    url TEXT
+);
+
+-- The status of an active opportunity.
+-- * Inactive: saved but not actively pursued
+-- * Researching: researching the company, role
+-- * Preparing: preparing the submission documents
+-- * Applied: documents have been submitted
+-- * Engaged: the employer initiated the hiring process
+-- * Ended: the opportunity has been followed through to its conclusion
+-- TODO: on each status change, require a journal entry?
+CREATE TYPE enterprise.opportunity_status AS ENUM (
+    'inactive',
+    'researching',
+    'preparing',
+    'applied',
+    'engaged',
+    'ended'
+);
+
+-- The result of an active opportunity.
+-- * Pending: the conclusion has not been reached
+-- * Ignored: the submission was not followed up
+-- * Deferred: the applicant ended it
+-- * Rejected: the target ended it
+-- * Offered: an offer was extended
+-- TODO: on each result change, require a journal entry?
+CREATE TYPE enterprise.opportunity_result AS ENUM (
+    'pending',
+    'ignored',
+    'deferred',
+    'rejected',
+    'offered'
+);
+
+-- An opportunity.
+CREATE TABLE enterprise.opportunity (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    employer UUID REFERENCES enterprise.employer,
+
+    -- TODO
+    -- Changing either of these should be done within a transaction which also
+    -- creates an entry in opportunity_event.
+    status enterprise.opportunity_status NOT NULL,
+    result enterprise.opportunity_result NOT NULL,
+
+    role TEXT,
+    location TEXT, -- TODO: handle remote
+    salary_range NUMRANGE,
+    post_body TEXT,
+    post_url TEXT,
+    technologies TEXT,
+
+    notes TEXT,
+
+    -- If the opportunity has ended, the result must not remain pending.
+    CONSTRAINT valid_result CHECK (status != 'ended' OR result != 'pending')
+);
+
+-- Opportunity-related contact.
+-- Feature: when receiving a call, quickly input number into auto-complete search,
+-- which will find the associated contact and pull up the associated opportunity,
+-- name, and other notes.
+-- TODO: Should this be associated with an employer instead?
+CREATE TABLE enterprise.opportunity_contact (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    opportunity UUID REFERENCES enterprise.opportunity,
+
+    name TEXT NOT NULL,
+    phone TEXT,
+    email TEXT,
+    notes TEXT
+);
+
+-- Opportunity-related, time-stamped journal entry.
+-- Examples: describing latest development, communication, etc.
+CREATE TABLE enterprise.opportunity_journal (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    opportunity UUID REFERENCES enterprise.opportunity,
+
+    content TEXT NOT NULL
+);
+
+-- Opportunity-related event.
+-- Examples: phone-screen datetime, onsite datetime, submission deadline.
+CREATE TABLE enterprise.opportunity_event (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    opportunity UUID REFERENCES enterprise.opportunity,
+
+    scheduled_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    description TEXT NOT NULL,
+    location TEXT -- TODO: handle remote
+);
+
+-- Opportunity-related resource url.
+-- Examples: topic to learn, interview experience, practice problem.
+-- TODO:
+-- * differentiate between categories?
+CREATE TABLE enterprise.opportunity_resource (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    opportunity UUID REFERENCES enterprise.opportunity,
+
+    url TEXT
+);
+
+-- Opportunity-related comment.
+-- TODO:
+-- * embed in database, or foreign reference?
+-- * differentiate between created and given?
+CREATE TABLE enterprise.opportunity_document (
+    uuid UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+
+    name TEXT NOT NULL,
+    encoding TEXT NOT NULL,
+    mime_type TEXT NOT NULL,
+    description TEXT,
+
+    content bytea,
+    foreign_content TEXT
+);
---
+++ db/test-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO enterprise.employer (name, location, url)
+VALUES ('Company', 'San Francisco, CA', 'http://www.somesite.com');
