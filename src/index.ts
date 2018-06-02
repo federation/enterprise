417d748cfef42ae2ac56d7fca1e329bcaf16e30d
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-06-02 11:52:28 -0700

    feat: graphql endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,67 @@
 'use strict';
 
-const Koa = require('koa');
+import Koa from 'koa';
+import koaRouter from 'koa-router';
+import koaBody from 'koa-bodyparser';
+import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
+import {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} from 'graphql';
+
+import { Client } from 'pg';
+
+const client = new Client();
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = new Koa();
+const router = new koaRouter();
 
-app.use((ctx: any) => {
-  ctx.body = 'Hello world';
+app.use(koaBody({
+  extendTypes: {
+    text: ['application/graphql']
+  }
+}));
+
+async function graphQLTextParser(ctx: Koa.Context, next: Function) {
+  if (ctx.request.is('application/graphql')) {
+    ctx.body = { query: ctx.request.body };
+  }
+
+  await next();
+}
+
+const myGraphQLSchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
+    }
+  })
 });
+
+router.post('/graphql', graphQLTextParser, graphqlKoa({ schema: myGraphQLSchema }));
+router.get('/graphql', graphqlKoa({ schema: myGraphQLSchema }));
+
+// NOTE
+// This endpoint should be disabled on production.
+router.get('/graphiql', graphiqlKoa({
+  endpointURL: '/graphql',
+  // passHeader: `'Authorization': 'Bearer lorem ipsum'`
+}));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT, HOST);
 
