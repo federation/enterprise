4a2bdeb62e95ddbbefd357baa37ee701eba72362
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-06-03 13:29:34 -0700

    feat: add Config class
    This consolidates all configuration and environment variables into one
    location so that it's easy to see at a glance, and enforce, the
    configuration of the application, rather than having different parts of
    the system implicitly depending on global configuration variables.
    
    Config is a singleton which caches process.env on construction. Required
    configuration values are then read into explicit instance variables with
    default values, optionally lazily computed. Those without default values
    are considered required and the application throws an error if they're
    missing.

---
+++ src/config.ts
@@ -0,0 +1,52 @@
+import path from 'path';
+
+export class Config {
+  static instance: Config;
+
+  private environment: any;
+
+  // TODO
+  // Use camelcase.
+  readonly port: number;
+  readonly host: string;
+  readonly node_env: string;
+  readonly log_path: string;
+  readonly jwt_secret: string;
+
+  private constructor(environment: any) {
+    this.environment = environment;
+
+    this.port = parseInt(this.env('PORT', '8080'));
+    this.host = this.env('HOST', '0.0.0.0');
+    this.node_env = this.env('NODE_ENV', 'development');
+    this.log_path = this.env('LOG_PATH', () => path.join(__dirname, '../logs'));
+
+    this.jwt_secret = this.env('JWT_SECRET');
+
+    Config.instance = this;
+  }
+
+  private env(key: string, orDefault?: (() => string) | string) {
+    if (typeof orDefault === 'string') {
+      return this.environment[key] || orDefault;
+    } else if (orDefault instanceof Function) {
+      return this.environment[key] || orDefault();
+    } else {
+      if (key in this.environment) {
+        return this.environment[key];
+      } else {
+        throw new Error(`Missing required environment variable: ${key}`);
+      }
+    }
+  }
+
+  static getInstance(environment: any = process.env) {
+    if (!Config.instance) {
+      Config.instance = new Config(environment);
+    }
+
+    return Config.instance;
+  }
+}
+
+export default Config.getInstance();
