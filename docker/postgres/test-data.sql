b260a30a5e8a9657e92d529b1eb2c96a1e3870ae
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-06-08 18:07:11 -0700

    style: move VALUES to end of INSERT line

--- docker/postgres/test-data.sql
+++ docker/postgres/test-data.sql
@@ -1,2 +1,5 @@
-INSERT INTO enterprise.employer (name, location, url)
-VALUES ('Company', 'San Francisco, CA', 'http://www.somesite.com');
+INSERT INTO enterprise.account (name, email, password, refresh_token) VALUES
+  ('admin', 'admin@localhost', 'hunter2', 'refr3sh');
+
+INSERT INTO enterprise.employer (name, location, url) VALUES
+  ('Company', 'San Francisco, CA', 'http://www.somesite.com');
