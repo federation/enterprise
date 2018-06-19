f9141c8c396038a20c6a97e353dc66f637aae2b3
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-06-18 17:16:15 -0700

    refactor(common): enforce keys of return type

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
-export function expectKeys<T extends object, K extends keyof T>(obj: T, ...keys: K[]) {
+export function expectKeys<T, K extends keyof T>(obj: any, ...keys: Array<K>): T {
   for (const key of keys) {
     if (!obj.hasOwnProperty(key)) {
       throw new Error(`Missing property for key: ${key}`);
