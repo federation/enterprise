ba19ad0f09c37312db184a7ed3562ff1440e49a1
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-06-19 23:23:20 -0700

    test: test db.connection and db.query

--- src/db/db.test.ts
+++ src/db/db.test.ts
@@ -1,9 +1,8 @@
-/* eslint-disable no-magic-numbers */
-/* eslint-disable import/imports-first */
-/* eslint-disable import/no-imports-first */
+jest.mock('pg');
+import pg from 'pg';
 
-import { QueryResult } from 'pg';
-
+jest.mock('../logger');
+import * as logger from '../logger';
 import * as db from './db';
 
 interface Person {
@@ -12,9 +11,72 @@ interface Person {
 }
 
 describe('db', () => {
+  const onMock = jest.fn();
+  const queryMock = jest.fn();
+
+  pg.Pool.mockImplementation(() => {
+    return {
+      query: queryMock,
+      on: onMock,
+    };
+  });
+
+  afterEach(() => {
+    queryMock.mockReset();
+    onMock.mockReset();
+    pg.Pool.mockReset();
+  });
+
+  describe('connection', () => {
+    test('it registers event handlers', () => {
+      // eslint-disable-next-line no-unused-vars
+      const pool = db.connection();
+
+      expect(onMock.mock.calls[0][0]).toEqual('acquire');
+      expect(onMock.mock.calls[1][0]).toEqual('remove');
+      expect(onMock.mock.calls[2][0]).toEqual('error');
+    });
+  });
+
+  describe('query', () => {
+    const infoMock = jest.fn();
+
+    logger.logger.mockImplementation(() => {
+      return {
+        info: infoMock,
+      };
+    });
+
+    afterEach(() => {
+      infoMock.mockReset();
+    });
+
+    test('it should perform and log the query', async () => {
+      const result = {
+        rowCount: 1,
+        rows: [{ name: 'bob', age: 18 }],
+      };
+
+      queryMock.mockResolvedValue(result);
+
+      const query = 'SELECT name FROM enterprise.accounts WHERE account_id = $1';
+      const params = [1];
+
+      await db.query(query, params);
+
+      expect(queryMock).toHaveBeenCalledWith(query, params);
+
+      expect(infoMock).toHaveBeenCalledWith('PostgreSQL Query', {
+        query,
+        params,
+        ...result,
+      });
+    });
+  });
+
   describe('expectRow', () => {
     test('it should get a single row', () => {
-      const singleRowResult: QueryResult = {
+      const singleRowResult: pg.QueryResult = {
         rows: [{ name: 'bob', age: 18 }],
         rowCount: 1,
         command: null,
@@ -29,7 +91,7 @@ describe('db', () => {
     });
 
     test('it should throw on more than one row', () => {
-      const multiRowResult: QueryResult = {
+      const multiRowResult: pg.QueryResult = {
         rows: [
           { name: 'bob', age: 18 },
           { name: 'alice', age: 19 },
@@ -49,7 +111,7 @@ describe('db', () => {
 
   describe('expectRows', () => {
     test('it should get multiple rows', () => {
-      const multiRowResult: QueryResult = {
+      const multiRowResult: pg.QueryResult = {
         rows: [
           { name: 'bob', age: 18 },
           { name: 'alice', age: 19 },
