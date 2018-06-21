8409b1aeaac88611bf15dc6c1cfc149825eb8ee0
Jorge Israel Peña <jorge.israel.p@gmail.com>
2018-06-20 18:09:56 -0700

    refactor: move koa server creation to server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,12 @@
 /* eslint-disable import/imports-first */
 require('dotenv').config();
 
-import Koa from 'koa';
-import koaBody from 'koa-bodyparser';
-import koaLogger from 'koa-logger';
-import Router from 'koa-router';
-
 import { config } from './config';
 import { logger } from './logger';
+import { server } from './server';
 
-import GraphQLRoutes from './routes/graphql';
+const app = server();
 
-import * as auth from './middleware/auth';
-
-logger().info('Starting Koa server', { host: config().HOST, port: config().PORT });
-
-const app = new Koa();
-
-app.use(koaLogger({
-  transporter: (str: any, args: any[]) => {
-    const [ , method, path, status, time, size ] = args;
-
-    if (!status) {
-      logger().info(`Requested ${method} ${path}`);
-    } else {
-      logger().info(`Responded ${method} ${path} ${status} ${time} ${size}`);
-    }
-  },
-}));
-
-// TODO: This runs before the request is actually finished. It'll consider a
-// request done as soon as reverse proxy starts receiving it? but the koa-logger
-// will consider it done until after nginx finishes responding.
-async function requestLogger(ctx: Koa.Context, next: Function) {
-  const start = Date.now();
-
-  await next();
-
-  const time = Date.now() - start;
-  const size = '0kb';
-
-  logger().info(`${ctx.method} ${ctx.path} ${ctx.status} ${time} ${size}`);
-}
-
-app.use(requestLogger);
-
-app.use(auth.unauthenticatedHandler);
-
-app.use(koaBody({
-  extendTypes: {
-    text: ['application/graphql'],
-  },
-}));
-
-app.use(GraphQLRoutes.routes());
-app.use(GraphQLRoutes.allowedMethods());
-
-app.listen(config().PORT, config().HOST);
-
-logger().info(`Running on http://${config().HOST}:${config().PORT}`);
+app.listen(config().PORT, config().HOST, () => {
+  logger().info(`Listening on http://${config().HOST}:${config().PORT}`);
+});
---
+++ src/server.ts
@@ -0,0 +1,54 @@
+import Koa from 'koa';
+import koaBody from 'koa-bodyparser';
+import koaLogger from 'koa-logger';
+
+import { logger } from './logger';
+
+import GraphQLRoutes from './routes/graphql';
+
+import * as auth from './middleware/auth';
+
+// TODO: This runs before the request is actually finished. It'll consider a
+// request done as soon as reverse proxy starts receiving it? but the koa-logger
+// will consider it done until after nginx finishes responding.
+async function requestLogger(ctx: Koa.Context, next: Function) {
+  const start = Date.now();
+
+  await next();
+
+  const time = Date.now() - start;
+  const size = '0kb';
+
+  logger().info(`${ctx.method} ${ctx.path} ${ctx.status} ${time} ${size}`);
+}
+
+export function server() {
+  const app = new Koa();
+
+  app.use(koaLogger({
+    transporter: (str: any, args: any[]) => {
+      const [ , method, path, status, time, size ] = args;
+
+      if (!status) {
+        logger().info(`Requested ${method} ${path}`);
+      } else {
+        logger().info(`Responded ${method} ${path} ${status} ${time} ${size}`);
+      }
+    },
+  }));
+
+  app.use(requestLogger);
+
+  app.use(auth.unauthenticatedHandler);
+
+  app.use(koaBody({
+    extendTypes: {
+      text: ['application/graphql'],
+    },
+  }));
+
+  app.use(GraphQLRoutes.routes());
+  app.use(GraphQLRoutes.allowedMethods());
+
+  return app;
+}
